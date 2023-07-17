1d945b1b2fc3a418a0b3de72bd229c2a5e4563c3
Dalia-Mohammed <139588795+Dalia-Mohammed@users.noreply.github.com>
2023-07-17 15:09:08 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,15 @@
+//  header- animation! 
+
+var typed = new Typed(" #element  " , {
+    strings: [  "  Designer.  ", " Larry Daniels .    ","Developer. "  ],
+    typeSpeed:50,
+    loop: true,
+    backDelay: 100,
+    backSpeed:60,
+  }
+  );
+
+
+
+
+
